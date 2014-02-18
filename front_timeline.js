b34ba286f5753e08fef432b42c78c2b7ea96140a
Alice-MacAir <amchung@stanford.edu>
2014-02-17 22:35:22 -0800

    block db test

---
+++ front_timeline.js
@@ -0,0 +1,146 @@
+const PORT = 3006;
+const HOST = '171.65.102.132';
+
+var express = require('express'),
+	http = require('http'),
+	server = http.createServer(app);
+	
+var app = express();
+
+const redis = require('redis');
+const client = redis.createClient();
+
+const io = require('socket.io');
+
+const list = redis.createClient();
+
+const _ = require('underscore');
+
+if (!module.parent) {
+    server.listen(PORT, HOST);
+    const socket  = io.listen(server);
+
+var rooms = ['arduino','lab'];
+
+socket.configure(function () {
+  //socket.set("transports", ["xhr-polling"]);
+  //socket.set("polling duration", 10);
+  //socket.set("close timeout", 10);
+  socket.set("log level", 1);
+});
+
+    socket.on('connection', function(client) {
+        const sub = redis.createClient();
+        sub.subscribe('realtime');
+        const pub = redis.createClient();
+        
+    	list.zrevrange("myset", 0 , 4, 'withscores', function(err,members){
+			var lists=_.groupBy(members,function(a,b){
+				return Math.floor(b/2);
+			});
+			console.log( _.toArray(lists) );
+			client.emit("postscore",  _.toArray(lists) );
+		});
+ 
+        sub.on("message", function(channel, message) {
+            client.send(message);
+        });
+        
+        client.on('message', function(msg) {
+        	switch(msg.type)
+			{
+				case "setUsername":
+  					pub.publish("realtime", "1&&"+"A New Challenger Enters the Ring:" + client.id +"  =  "+ msg.user);
+  					break;
+				case "sendscore":
+  					list.zadd("myset", msg.score , msg.user);
+					list.zrevrange("myset", 0 , 4, 'withscores', function(err,members){
+						var lists=_.groupBy(members,function(a,b){
+							return Math.floor(b/2);
+						});
+						console.log( _.toArray(lists) );
+						client.emit("postscore",  _.toArray(lists) );
+					});
+  					break;
+  				case "chat":
+  					pub.publish("realtime", "1&&"+msg.message);
+  					break;
+  				case "sendarrow":
+					pub.publish("realtime", "0&&"+msg.led1+"^"+msg.led2+"^"+msg.led3+"^"+msg.led4);
+  					break;
+  				case "sendvalveopen":
+  					pub.publish("realtime", "1&&"+"Valve triggered.");
+  					break;
+				//default:
+  				//	console.log("!!!received unknown input msg!!!");
+			}
+        });
+        
+        client.on('timeline',function(msg){
+        	// get max limit
+        	var timeline_end = list.get("global:next_tb_id");
+        	
+        	switch(msg.type)
+        	{
+        		case "callblocks":
+        			// convert dates and get block ids
+        			var begintime = msg.begintime;
+        			var endtime = msg.endtime;
+        			
+        			console.log(begintime);
+        			console.log(endtime);
+        			
+        			var firstid = list.get("tb_time:"+begintime+":tb_id");
+        			var lastid = list.get("tb_time:"+endtime+":tb_id");
+        			
+        			// if out of range
+        			if (firstid==null){
+        				firstid = 0;
+        				lastid = firstid + (24*60/5);
+        			}
+        			if (lastid==null)
+        			{
+        				lastid = timeline_end;
+        				firstid = lastid - +(24*60/5);
+        			}
+        			console.log(firstid);
+        			console.log(lastid);
+        			
+        			var JSONData = [];
+        			// get blocks
+        			for (var i=firstid;i=<lastid;i++){
+        				
+        				/*var block;
+        				block.id = i;
+						block.time = list.get("tb_id:"+i+":time");
+						block.lock = list.get("tb_id:"+i+":locked");
+						block.userid = list.get("tb_id:"+i+":userid");
+						block.expid = list.get("tb_id:"+i+":expid");*/
+						
+						JSONData.push({
+							"id": i, 
+							"time": list.get("tb_id:"+i+":time"),
+							"lock": list.get("tb_id:"+i+":locked"),
+							"userid": list.get("tb_id:"+i+":userid"),
+							"expid": list.get("tb_id:"+i+":expid")
+							});
+					}
+					
+					// emit results
+					client.emit("postblocks",  JSONData );
+        			break;
+        		case "writeblocks":
+        			// convert dates and get block ids
+        			var targetdate = msg.targetdate;
+        			var targetid = list.get("tb_time:"+targetdate+":tb_id");
+        			break;
+        	}
+        });
+        
+        client.on('disconnect', function() {
+            sub.quit();
+            pub.publish("realtime","Disconnected :" + client.id);
+        });
+    });
+}
+
