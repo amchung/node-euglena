3c8c4f70d2a0fc9ef2db6b471501744b82ba8911
Alice-MacAir <amchung@stanford.edu>
2014-02-28 23:05:51 -0800

    current time block

--- front_timeline_asynch_clock.js
+++ front_timeline_asynch_clock.js
@@ -238,6 +238,7 @@ function markTimeblock(){
 	var start = Date.UTC(2014,02,11,0);
 	//var end = Date.UTC(2014,07,01,0);
 	var end = new Date();
+	end = end.getTime();
 	var blocks = (end-start)/block;
 	
 	console.log(start);
