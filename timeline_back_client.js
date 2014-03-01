bde5809a4d2aaefcfa47289c0f825b11fe11522f
Alice-MacAir <amchung@stanford.edu>
2014-03-01 15:11:31 -0800

    back/front for timeline

--- timeline_back_client.js
+++ timeline_back_client.js
@@ -27,7 +27,7 @@ socket.on('tic', function(data){
 });
 
 socket.on('snapshot', function(data){
-	var current_block_id = data;
+	current_block_id = data;
   	var imgpath = takeSnapshot();
   	console.log(current_block_id);
 });
