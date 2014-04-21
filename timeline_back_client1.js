6c010c86c41b4e3947e3d8d84540d23c51722125
Alice-MacAir <amchung@stanford.edu>
2014-04-21 13:33:21 -0700

    archiver

--- timeline_back_client1.js
+++ timeline_back_client1.js
@@ -28,9 +28,9 @@ socket.on('stoprecordblock-clients', function(){
 });
 
 function archiveImages () {
-	var dir_name = path.join('../../Dropbox','live-gallery',current_block_id);
-	//var file_name = path.join('../../Dropbox','live-gallery',current_block_id,"block_"+current_block_id.toString()+"_images.zip");
-	var output = fs.createWriteStream("block_"+current_block_id.toString()+"_images.zip");
+	var dir_name = path.join('../../Dropbox','zip',current_block_id);
+	var file_name = path.join('../../Dropbox','zip',"block_"+current_block_id.toString()+"_images.zip");
+	var output = fs.createWriteStream(file_name);
 	var archive = archiver('zip');
 	
 	output.on('close', function(){
@@ -44,7 +44,7 @@ function archiveImages () {
 	
 	archive.pipe(output);
 	archive.bulk([
-		{ expand: true, cwd: dir_name , src: ['**'], dest: dir_name }
+		{ expand: true, cwd: dir_name , src: ['*.jpg'] }
 	]);
 	archive.finalize();
 }
