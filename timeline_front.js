72fe7fb0c284ddd217c491d93978621f1e81b96f
Alice-MacAir <amchung@stanford.edu>
2014-07-23 00:00:26 -0400

    tb_maker for new DB

--- timeline_front.js
+++ timeline_front.js
@@ -208,7 +208,7 @@ var now = new Date();
 
 function markTimeblock(){
 	var block = 5*60*1000;
-	var start = Date.UTC(2014,04,1);
+	var start = Date.UTC(2014,06,1);
 	var end = new Date();
 	end = end.getTime();
 	var blocks = Math.floor((end-start)/block);
